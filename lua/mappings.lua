7bd97f74dcea292891b0a8a16a4e653078246330
PyDev19 <atharvamishra956+1@gmail.com>
2023-06-01 20:10:10 -0500

    Added keybindings to open terminal
    Added keybinds for telescope git

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,12 +1,21 @@
-local map = vim.api.nvim_set_keymap
+local map = vim.api.nvim_set_keymapVG
+
 local options = { noremap = true, silent = true }
 
+-- Define a function to toggle the terminal
+function toggle_terminal()
+	require("nvterm.terminal").toggle("float")
+end
+
 -- Normal Mode Keybinds
+-- Git
+map("n", "<leader>g", ":Telescope git_status<CR>", options)
+map("n", "<leader>gb", ":Telescope git_branches<CR>", options)
+map("n", "<leader>gc", ":Telescope git_commits<CR>", options)
+map("n", "<leader>gf", ":Telescope git_files<CR>", options)
 map("n", "<C-p>", ":Telescope find_files<CR>", options)
 map("n", "<esc>", ":noh<CR>", options)
 map("n", "<C-s>", ":w!<CR>", options)
-map("n", "<leader>g", ":Telescope git_status<CR>", options)
-map("n", "<leader>gb", ":Telescope git_branches<CR>", options)
 map("n", "<C-z>", "u<CR>", options)
 map("n", "<C-y>", "U<CR>", options)
 map("n", "<leader>th", ":Telescope colorscheme<CR>", options)
@@ -18,7 +27,8 @@ map("n", "<leader>e", ":NvimTreeFocus<CR>", options)
 map("n", "<C-w>", ":BufferClose<CR>", options)
 map("n", "<A-w>", ":BufferRestor<CR>", options)
 map("n", "<leader>fm", ":lua vim.lsp.buf.format({ async = true })<CR>", options)
-map("n", "<C-t>", ":ToggleTerm direction=float<CR>", options)
+map("n", "<C-t>", ":lua toggle_terminal()<CR>", options)
+map("n", "<tab>", ":BufferNext<CR>", options)
 
 -- Insert Mode Keybinds
 map("i", "<C-z>", "<C-o>u<CR>", options)
@@ -31,7 +41,7 @@ map("i", "<C-e>", "<esc>:NvimTreeFocus<CR>", options)
 map("i", "<C-w>", "<C-o>:BufferClose<CR>", options)
 map("i", "<A-w>", "<C-o>:BufferRestore<CR>", options)
 map("i", "<C-f>", "<C-o>:/", { noremap = true })
-map("i", "<C-t>", "<C-o>:ToggleTerm direction=float<CR>", options)
+map("i", "<C-t>", "<C-o>:lua toggle_terminal()<CR>", options)
 
 -- Visual Mode
 map("v", "<S-Up>", ":m '<-2<CR>gv=gv", options)
@@ -39,4 +49,6 @@ map("v", "<S-Down>", ":m '>+1<CR>gv=gv", options)
 map("v", "<tab>", ">", options)
 map("v", "<S-tab>", "<", options)
 map("v", "<C-/>", "gc", options)
-map("n", "<tab>", ":BufferNext<CR>", options)
+
+-- Terminal
+map("t", "<C-t>", "<C-\\><C-n>:lua toggle_terminal()<CR>", options)
