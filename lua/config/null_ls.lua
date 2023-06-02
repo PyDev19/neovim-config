9b20822c2ccf6083dff3ce2a0a9589a982abded9
PyDev19 <atharvamishra956+1@gmail.com>
2023-06-02 11:03:04 -0500

    Removed markdown linter and formatter Using treesitter for it instead

--- lua/config/null_ls.lua
+++ lua/config/null_ls.lua
@@ -5,10 +5,8 @@ local lint = null_ls.builtins.diagnostics
 local sources = {
 	lint.selene,
 	lint.flake8.with({ extra_args = { "--max-line-length", "500" } }),
-	lint.markdownlint,
 	formatting.stylua,
 	formatting.black.with({ extra_args = { "--line-length=120" } }),
-	formatting.prettier,
 }
 
 null_ls.setup({
