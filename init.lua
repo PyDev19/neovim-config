1cd251ec2c20e230ef957af0d934d98e6017d136
PyDev19 <atharvamishra956+1@gmail.com>
2023-06-03 14:25:20 -0500

    Added config for neovide

--- init.lua
+++ init.lua
@@ -3,3 +3,12 @@ require("settings")
 require("mappings")
 require("config.completion")
 require("config.lsp")
+
+-- for neovide
+if vim.g.neovide then
+	vim.o.guifont = "JetBrains Mono Regular:h11"
+	vim.g.neovide_cursor_animation_length = 0
+	vim.cmd([[
+		autocmd VimEnter * silent! lcd %:p:h
+	]])
+end
