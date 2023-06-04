ef58fcc7d1704e9ed543f9c2d830ae3b668c086b
PyDev19 <atharvamishra956+1@gmail.com>
2023-06-03 20:58:03 -0500

    Fixed neovide not changing fonts

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ require("config.lsp")
 
 -- for neovide
 if vim.g.neovide then
-	vim.o.guifont = "JetBrains Mono Regular:h11"
+	vim.o.guifont = "JetBrains Mono:h11"
 	vim.g.neovide_cursor_animation_length = 0
 	vim.cmd([[
 		autocmd VimEnter * silent! lcd %:p:h
