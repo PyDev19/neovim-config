0e75b1fca945ef48bb938b5f8d200b3232e3256d
PyDev19 <atharvamishra956+1@gmail.com>
2023-06-03 19:48:31 -0500

    Added settings for code folding

--- lua/settings.lua
+++ lua/settings.lua
@@ -19,3 +19,9 @@ vim.opt.termguicolors = true
 -- Set line number options
 vim.wo.number = true
 vim.wo.relativenumber = false
+
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
