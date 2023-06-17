026d4c4b902aca86c910315f719b9ef11a7578d9
PyDev19 <atharvamishra956+1@gmail.com>
2023-06-17 15:36:08 -0500

    Add diagnostic options to nvim-tree

--- lua/config/nvimtree.lua
+++ lua/config/nvimtree.lua
@@ -44,6 +44,22 @@ local options = {
 			},
 		},
 	},
+	diagnostics = {
+        enable = true,
+        show_on_dirs = false,
+        show_on_open_dirs = true,
+        debounce_delay = 50,
+        severity = {
+          min = vim.diagnostic.severity.HINT,
+          max = vim.diagnostic.severity.ERROR,
+        },
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
+        },
+	}
 }
 
 return options
