c6784b4debb4ed6f3550564f9501699a7e1ac99e
PyDev19 <atharvamishra956+1@gmail.com>
2023-06-17 15:35:37 -0500

    Added setting to disable line numbers in terminal

--- lua/settings.lua
+++ lua/settings.lua
@@ -25,3 +25,6 @@ vim.o.foldcolumn = "1"
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+-- disable line numbers in terminal
+vim.cmd([[autocmd TermOpen * setlocal nonumber]])
