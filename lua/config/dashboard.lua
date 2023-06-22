0d2162e41a301b0ea21e9b1c8e324e57c0b6b15f
PyDev19 <atharvamishra956+1@gmail.com>
2023-06-21 19:28:54 -0500

    Switched from dashboard.nvim to alpha.nvim

--- lua/config/dashboard.lua
+++ lua/config/dashboard.lua
@@ -1,54 +1,36 @@
-local header = {
-	"███╗   ██╗███████╗ ██████╗     ███████╗████████╗██╗   ██╗██████╗ ██╗ ██████╗",
-	"████╗  ██║██╔════╝██╔═══██╗    ██╔════╝╚══██╔══╝██║   ██║██╔══██╗██║██╔═══██╗",
-	"██╔██╗ ██║█████╗  ██║   ██║    ███████╗   ██║   ██║   ██║██║  ██║██║██║   ██║",
-	"██║╚██╗██║██╔══╝  ██║   ██║    ╚════██║   ██║   ██║   ██║██║  ██║██║██║   ██║",
-	"██║ ╚████║███████╗╚██████╔╝    ███████║   ██║   ╚██████╔╝██████╔╝██║╚██████╔╝",
-	"╚═╝  ╚═══╝╚══════╝ ╚═════╝     ╚══════╝   ╚═╝    ╚═════╝ ╚═════╝ ╚═╝ ╚═════╝",
+local dashboard = require("alpha.themes.dashboard")
+local config_path = vim.fn.stdpath("config")
+local logo = [[
+	 /$$   /$$                            /$$$$$$    /$$                     /$$ /$$
+	| $$$ | $$                           /$$__  $$  | $$                    | $$|__/
+	| $$$$| $$  /$$$$$$   /$$$$$$       | $$  \__/ /$$$$$$   /$$   /$$  /$$$$$$$ /$$  /$$$$$$
+	| $$ $$ $$ /$$__  $$ /$$__  $$      |  $$$$$$ |_  $$_/  | $$  | $$ /$$__  $$| $$ /$$__  $$
+	| $$  $$$$| $$$$$$$$| $$  \ $$       \____  $$  | $$    | $$  | $$| $$  | $$| $$| $$  \ $$
+	| $$\  $$$| $$_____/| $$  | $$       /$$  \ $$  | $$ /$$| $$  | $$| $$  | $$| $$| $$  | $$
+	| $$ \  $$|  $$$$$$$|  $$$$$$/      |  $$$$$$/  |  $$$$/|  $$$$$$/|  $$$$$$$| $$|  $$$$$$/
+	|__/  \__/ \_______/ \______/        \______/    \___/   \______/  \_______/|__/ \______/
+]]
+
+dashboard.section.header.val = vim.split(logo, "\n")
+
+dashboard.section.buttons.val = {
+	dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+	dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+	dashboard.button("p", "󰃀 " .. " Open Projects", ":Telescope project <CR>"),
+	dashboard.button("c", " " .. " Config", ":e $MYVIMRC | cd " .. config_path .. "<CR>"),
+	dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load({last = true}) <cr>]]),
+	dashboard.button("l", "⚡" .. " Lazy", ":Lazy<CR>"),
+	dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 
-local config_path = vim.fn.stdpath('config')
+for _, button in ipairs(dashboard.section.buttons.val) do
+	button.opts.hl = "AlphaButtons"
+	button.opts.hl_shortcut = "AlphaShortcut"
+end
 
-local options = {
-	theme = "hyper",
-	shortcut_type = "number",
-	change_to_vcs_root = false,
-	hide = {
-		statusline = false,
-		winbar = false,
-	},
-	config = {
-		header = header,
-		packages = { enable = false },
-		shortcut = {
-			{
-				icon = "󰊳 ",
-				desc = "Update",
-				action = "PackerSync",
-				key = "u",
-			},
-			{
-				icon = " ",
-				icon_hl = "@variable",
-				desc = "Find Files",
-				group = "Label",
-				action = "Telescope fd",
-				key = "f",
-			},
-			{
-				icon = "$ ",
-				desc = "Open Terminal",
-				action = "lua toggle_terminal(1)",
-				key = "t",
-			},
-			{
-				icon = " ",
-				desc = "Open Config",
-				action = "e $MYVIMRC | cd " .. config_path,
-				key = "c",
-			},
-		},
-	},
-}
+dashboard.section.header.opts.hl = "AlphaHeader"
+dashboard.section.buttons.opts.hl = "AlphaButtons"
+dashboard.section.footer.opts.hl = "AlphaFooter"
+dashboard.opts.layout[1].val = 8
 
-return options
+return dashboard
