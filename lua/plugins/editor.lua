ad6a7755f6714bb81d24a5e7a224ed92fc4c250b
PyDev19 <atharvamishra956+1@gmail.com>
2023-06-21 19:22:39 -0500

    Created file for editor related plugins

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,33 @@
+local plugins = {
+    {
+		"mg979/vim-visual-multi",
+		event = "BufRead",
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "InsertEnter",
+		config = function()
+			local options = require("config.indent_blankline")
+			require("indent_blankline").setup(options)
+		end,
+	},
+	{
+		"numToStr/Comment.nvim",
+		event = "BufRead",
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({
+				fast_wrap = {},
+				disable_filetype = { "TelescopePrompt", "vim" },
+			})
+		end,
+	},
+}
+
+return plugins
