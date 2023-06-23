ab7a71564d22bef8e25532428a5dc7e24d523748
PyDev19 <atharvamishra956+1@gmail.com>
2023-06-22 22:28:27 -0500

    Added git and diagnostic icons for barbar

--- lua/config/tabbar.lua
+++ lua/config/tabbar.lua
@@ -5,6 +5,20 @@ local options = {
 	sidebar_filetypes = {
 		NvimTree = { text = "       File Explorer" },
 	},
+	icons = {
+		button = "",
+		diagnostics = {
+			[vim.diagnostic.severity.ERROR] = { enabled = true, icon = " " },
+			[vim.diagnostic.severity.WARN] = { enabled = true, icon = " " },
+			[vim.diagnostic.severity.INFO] = { enabled = true, icon = " " },
+			[vim.diagnostic.severity.HINT] = { enabled = true, icon = " " },
+		},
+		gitsigns = {
+			added = { enabled = true, icon = "+" },
+			changed = { enabled = true, icon = "~" },
+			deleted = { enabled = true, icon = "-" },
+		},
+	},
 }
 
 return options
