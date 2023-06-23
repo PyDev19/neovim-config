22dabefce78e57ff84b9c4f43f580a52ec44feab
PyDev19 <atharvamishra956+1@gmail.com>
2023-06-22 22:26:30 -0500

    Added for themes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,15 +2,56 @@
 local plugins = {
 	{
 		"folke/tokyonight.nvim",
-		config = function ()
-			vim.cmd("colorscheme tokyonight-night")
-		end
+		lazy = true,
+		config = function()
+			local options = require("config.themes").get_theme_opts(1)
+			require("tokyonight").setup(options)
+		end,
 	},
 	{
 		"navarasu/onedark.nvim",
+		lazy = true,
 		config = function()
-			require("config.themes")
+			local options = require("config.themes").get_theme_opts(2)
+			require("onedark").setup(options)
 		end,
+	},
+	{
+		"olimorris/onedarkpro.nvim",
+		lazy = true,
+		config = function()
+			local options = require("config.themes").get_theme_opts(3)
+			require("onedarkpro").setup(options)
+			require("onedarkpro").load()
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = true,
+		config = function()
+			local options = require("config.themes").get_theme_opts(4)
+			require("gruvbox").setup(options)
+			require("gruvbox").load()
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = true,
+		config = function()
+			local options = require("config.themes").get_theme_opts(5)
+			require("kanagawa").setup(options)
+			require("kanagawa").load()
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function ()
+			local options = require("config.themes").get_theme_opts(6)
+			require("catppuccin").setup(options)
+			require("catppuccin").load()
+		end
 	},
 }
 
