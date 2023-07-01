1bcdbad79a2ab4558ccdae639c901cd083451399
PyDev19 <atharvamishra956+1@gmail.com>
2023-07-01 16:41:06 -0500

    editted completion keybinds

--- lua/config/lsp/completion.lua
+++ lua/config/lsp/completion.lua
@@ -4,11 +4,18 @@ require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
 	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Down>"] = cmp.mapping.scroll_docs(-4),
+		["<C-Up>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+		end,
 	}),
 	snippet = {
 		expand = function(args)
