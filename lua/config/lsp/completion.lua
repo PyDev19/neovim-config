77c57395125208ffe0a6f9d19f9aa43d38b3bf73
PyDev19 <atharvamishra956+1@gmail.com>
2023-07-03 12:11:04 -0500

    changed look of completion

--- lua/config/lsp/completion.lua
+++ lua/config/lsp/completion.lua
@@ -1,8 +1,41 @@
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+local lspkind = require("lspkind")
+local cmp_window = require("cmp.config.window")
 
-require("luasnip.loaders.from_vscode").lazy_load()
+local function has_words_before()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 
 cmp.setup({
+	preselect = cmp.PreselectMode.None,
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		format = lspkind.cmp_format({
+			mode = "symbol_text",
+			maxwidth = 100,
+			ellipsis_char = "...",
+
+			preset = "codicons",
+
+			before = function(entry, vim_item)
+				return vim_item
+			end,
+		}),
+	},
+	duplicates = {
+		nvim_lsp = 1,
+		luasnip = 1,
+		cmp_tabnine = 1,
+		buffer = 1,
+		path = 1,
+	},
+	window = {
+		completion = cmp_window.bordered(),
+		documentation = cmp_window.bordered(),
+	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-Down>"] = cmp.mapping.scroll_docs(-4),
 		["<C-Up>"] = cmp.mapping.scroll_docs(4),
@@ -12,6 +45,10 @@ cmp.setup({
 		["<Tab>"] = function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
 			else
 				fallback()
 			end
@@ -23,9 +60,9 @@ cmp.setup({
 		end,
 	},
 	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-	}, {
-		{ name = "buffer" },
+		{ name = "nvim_lsp", priority = 1000 },
+		{ name = "luasnip",  priority = 750 },
+		{ name = "buffer",   priority = 500 },
+		{ name = "path",     priority = 250 },
 	}),
 })
