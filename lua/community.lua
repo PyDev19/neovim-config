a5be84967c66728230daa167a30b932c9ec8a7d3
PyDev19 <atharvamishra956+1@gmail.com>
2024-07-09 19:48:01 -0500

    Used builtin community file

--- lua/community.lua
+++ lua/community.lua
@@ -6,5 +6,6 @@
 return {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.pack.lua" },
+    { import = "astrocommunity.recipes.neovide" },
     -- import/override with your plugins folder
 }
